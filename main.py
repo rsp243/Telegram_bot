4d1f652e4fabf4ace3580a80bf63ca4feb02a587
rsp243 <reshetov.semjon@gmail.com>
2021-01-16 18:34:38 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import telebot, requests
+from telebot import types
+import crids
+
+quest = crids.jsonToQuest
+ans = crids.jsonToAns
+
+bot = telebot.TeleBot(crids.token)
+
+
+def formMarkup(id):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    a = list()
+    for i in quest[id]['answers']:
+        for j in range(len(ans)):
+            if i == ans[j]['id']:
+                item = types.KeyboardButton(ans[j]['text'])
+        a.append(i)
+        markup.add(item)
+    return markup
+
+
+def sendMessage(id, message):
+    keyboard = formMarkup(id)
+    if quest[id]["message_before_question"] != '':
+        bot.send_message(message.chat.id, f'{quest[id]["message_before_question"]}')
+    bot.reply_to(message, f'{quest[id]["text"]}', reply_markup=keyboard)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    id = 0
+    sendMessage(id, message)
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    try:
+        if message.chat.type == 'private':
+            for i in range(len(ans)):
+                if ans[i]['text'] == message.text:
+                    sendMessage(ans[i]['goto'] - 1, message)
+    except:
+        bot.send_message(message.chat.id, 'Что-то пошло не так, обратитесь через почту к создателю этого бота -> reshetov.semjon@gmail.com')
+
+
+bot.polling()
