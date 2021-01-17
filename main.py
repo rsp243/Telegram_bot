3b0c0331cb9f700471db78053edd1022d3163d0a
rsp243 <reshetov.semjon@gmail.com>
2021-01-17 09:30:09 +0400

    The last

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import telebot, requests
 from telebot import types
-import os
+import crids
 
-quest = requests.get(os.environ['jsonToQuest']).json()
-ans = requests.get(os.environ['jsonToAns']).json()
+quest = crids.jsonToQuest
+ans = crids.jsonToAns
 
-bot = telebot.TeleBot(os.environ['token'])
+bot = telebot.TeleBot(crids.token)
 
 
 def formMarkup(id):
@@ -18,31 +18,40 @@ def formMarkup(id):
     return markup
 
 
+keyboardStart = types.ReplyKeyboardMarkup(resize_keyboard=True)
+keyboardStart.add('В самое начало')
+
+
 def sendMessage(id, message):
-    keyboard = formMarkup(id)
-    for j in range(len(ans)):
-        if ans[j]['id'] == id:
-            id = j + 1
-            if quest[id]["message_before_question"] != '':
-                bot.send_message(message.chat.id, f'{quest[id]["message_before_question"]}')
-            bot.reply_to(message, f'{quest[id]["text"]}', reply_markup=keyboard)
+    k = 0
+    for j in range(len(quest)):
+        if quest[j]['id'] == id:
+            k += 1
+            keyboard = formMarkup(j)
+            if quest[j]["message_before_question"] != '':
+                bot.send_message(message.chat.id, f'{quest[j]["message_before_question"]}')
+            bot.reply_to(message, f'{quest[j]["text"]}', reply_markup=keyboard)
+    if k == 0:
+        bot.send_message(message.chat.id, 'Диалог дальше ещё не сформирован,'
+                                          ' администраторы уже работают над этим, обратитесь через почту'
+                                          ' к создателю этого бота -> reshetov.semjon@gmail.com',
+                         reply_markup=keyboardStart)
 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    id = 2
+    id = 1
     sendMessage(id, message)
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    try:
-        if message.chat.type == 'private':
-            for i in range(len(ans)):
-                if ans[i]['text'] == message.text:
-                    sendMessage(ans[i]['goto'] - 1, message)
-    except:
-        bot.send_message(message.chat.id, 'Диалог дальше ещё не сформирован, администраторы уже работают над этим, обратитесь через почту к создателю этого бота -> reshetov.semjon@gmail.com')
+    if message.chat.type == 'private':
+        for i in range(len(ans)):
+            if ans[i]['text'] == message.text:
+                sendMessage(ans[i]['goto'], message)
+        if message.text == 'В самое начало':
+            sendMessage(1, message)
 
 
 bot.polling()
